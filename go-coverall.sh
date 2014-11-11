21ec00cc55397992ba0a753c1f8152fce4b7ad6b
ikawaha <ikawaha@users.noreply.github.com>
2014-11-11 18:39:37 +0900

    fixed coverage settings

--- go-coverall.sh
+++ go-coverall.sh
@@ -13,7 +13,7 @@ else
 fi
  
 XARGS_ARG=DIR
- 
+
 CMD_COV="go test $XARGS_ARG -covermode=$COV_MODE -coverprofile=$XARGS_ARG/$COV_PARTIAL_FILE"
 CMD_CHECKDIR="ls $XARGS_ARG/$COV_PARTIAL_FILE > /dev/null 2>&1"
 CMD_CONCATCOV="cat $XARGS_ARG/$COV_PARTIAL_FILE | grep -v '$HEADER' >> $COV_FILE"
@@ -22,6 +22,6 @@ GOVERALLS=$(echo $GOPATH | tr ':' '\n' | head -n 1)/bin/goveralls
  
 echo $HEADER > $COV_FILE
  
-find ./*  -maxdepth 10 -name '*.go' -print0 | $XARGS -0 -L1 dirname | uniq | $XARGS -I$XARGS_ARG sh -c "$CMD_COV && ($CMD_CHECKDIR && $CMD_CONCATCOV) || exit 0"
+find ./*  -maxdepth 10 -name '*.go' -print0 | $XARGS -0 -L1 dirname | uniq | grep -v "/_" | $XARGS -I$XARGS_ARG sh -c "$CMD_COV && ($CMD_CHECKDIR && $CMD_CONCATCOV) || exit 0"
  
 $GOVERALLS -coverprofile=$COV_FILE -service=travis-ci
